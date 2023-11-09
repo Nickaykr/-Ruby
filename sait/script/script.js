e00c23305a8ffbe74113aec84f7b56d0d0eee265
Nickaykr <150320779+Nickaykr@users.noreply.github.com>
2023-11-09 09:04:48 +0300

    Add files via upload

---
+++ sait/script/script.js
@@ -0,0 +1,157 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var modal = document.getElementById("myModal");
+  var modal2 = document.getElementById("myModal2");
+  var modal3 = document.getElementById("myModal3");
+  var modal4 = document.getElementById("myModal4");
+  var modal5 = document.getElementById("myModal5");
+  var modal6 = document.getElementById("myModal6");
+  var modal7 = document.getElementById("myModal7");
+  var modal8 = document.getElementById("myModal8");
+  var modal9 = document.getElementById("myModal9");
+  var modal10 = document.getElementById("myModal10");
+  var modal11 = document.getElementById("myModal11");
+  var modal12 = document.getElementById("myModal12");
+  var modal13 = document.getElementById("myModal13");
+  var modal14 = document.getElementById("myModal14");
+  var modal15 = document.getElementById("myModal15");
+
+  var btn = document.getElementById("addmoney");
+  var btn1 = document.getElementById("anime");
+  var btn2 = document.getElementById("avatar");
+  var btn3 = document.getElementById("balance");
+  var btn42 = document.getElementById("clear");
+  var btn4 = document.getElementById("createrol");
+  var btn5 = document.getElementById("interaction");
+  var btn6 = document.getElementById("jokes");
+  var btn7 = document.getElementById("ping");
+  var btn8 = document.getElementById("profile");
+  var btn9 = document.getElementById("rolebuy");
+  var btn10 = document.getElementById("searchforplayes");
+  var btn11 = document.getElementById("top");
+  var btn12 = document.getElementById("transfer");
+  var btn13 = document.getElementById("work");
+
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
+
+  btn1.onclick = function() {
+    modal2.style.display = "block";
+  }
+  btn2.onclick = function() {
+    modal3.style.display = "block";
+  }
+  btn3.onclick = function() {
+    modal4.style.display = "block";
+  }
+  btn4.onclick = function() {
+    modal5.style.display = "block";
+  }
+  btn5.onclick = function() {
+    modal6.style.display = "block";
+  }
+  btn6.onclick = function() {
+    modal7.style.display = "block";
+  }
+  btn7.onclick = function() {
+    modal8.style.display = "block";
+  }
+  btn8.onclick = function() {
+    modal9.style.display = "block";
+  }
+  btn9.onclick = function() {
+    modal10.style.display = "block";
+  }
+  btn10.onclick = function() {
+    modal11.style.display = "block";
+  }
+  btn11.onclick = function() {
+    modal12.style.display = "block";
+  }
+  btn12.onclick = function() {
+    modal13.style.display = "block";
+  }
+  btn13.onclick = function() {
+    modal14.style.display = "block";
+  }
+  btn42.onclick = function() {
+    modal15.style.display = "block";
+  }
+
+  window.closeModal = function(event) {
+    if (event.target.id === 'myModal') {
+      modal.style.display = 'none';
+    }
+  }
+
+  window.closeModal2 = function(event) {
+    if (event.target.id === 'myModal2') {
+      modal2.style.display = 'none';
+    }
+  }
+  window.closeModal3 = function(event) {
+    if (event.target.id === 'myModal3') {
+      modal3.style.display = 'none';
+    }
+  }
+  window.closeModal4 = function(event) {
+    if (event.target.id === 'myModal4') {
+      modal4.style.display = 'none';
+    }
+  }
+  window.closeModal5 = function(event) {
+    if (event.target.id === 'myModal5') {
+      modal5.style.display = 'none';
+    }
+  }
+  window.closeModal6 = function(event) {
+    if (event.target.id === 'myModal6') {
+      modal6.style.display = 'none';
+    }
+  }
+  window.closeModal7 = function(event) {
+    if (event.target.id === 'myModal7') {
+      modal7.style.display = 'none';
+    }
+  }
+  window.closeModal8 = function(event) {
+    if (event.target.id === 'myModal8') {
+      modal8.style.display = 'none';
+    }
+  }
+  window.closeModal9 = function(event) {
+    if (event.target.id === 'myModal9') {
+      modal9.style.display = 'none';
+    }
+  }
+  window.closeModal10 = function(event) {
+    if (event.target.id === 'myModal10') {
+      modal10.style.display = 'none';
+    }
+  }
+  window.closeModal11 = function(event) {
+    if (event.target.id === 'myModal11') {
+      modal11.style.display = 'none';
+    }
+  }
+  window.closeModal12 = function(event) {
+    if (event.target.id === 'myModal12') {
+      modal12.style.display = 'none';
+    }
+  }
+  window.closeModal13 = function(event) {
+    if (event.target.id === 'myModal13') {
+      modal13.style.display = 'none';
+    }
+  }
+  window.closeModal14 = function(event) {
+    if (event.target.id === 'myModal14') {
+      modal14.style.display = 'none';
+    }
+  }
+  window.closeModal15 = function(event) {
+    if (event.target.id === 'myModal15') {
+      modal15.style.display = 'none';
+    }
+  }
+});
